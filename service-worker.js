c6372927837637a3cdb7f1323fa7501bb554033c
k10tad <hollywat34@gmail.com>
2026-08-26 02:51:19 +0900

    Update Meridian

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Commander image refresh
 //========================
 
-const MERIDIAN_SW_VERSION = "meridian-runtime-5.2.0-classified-dossiers";
+const MERIDIAN_SW_VERSION = "meridian-runtime-5.2.1-ios-fixed-navigation";
 const MERIDIAN_ROOT = new URL("./", self.location.href).pathname;
 
 self.addEventListener("install", function () {
